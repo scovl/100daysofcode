9aa0cd76689530252c7dfcea63e2644eb9c41718
Vitor Lobo Ramos <lobocode@gmail.com>
2024-04-23 10:54:22 -0300

    add math_ops header and src

---
+++ c/tpt/include/math_ops.h
@@ -0,0 +1,7 @@
+#ifndef _MATH_OPS_H_
+#define _MATH_OPS_H_
+
+int sum(int a, int b);
+int factorial(int n);
+
+#endif
---
+++ c/tpt/src/math_ops.c
@@ -0,0 +1,10 @@
+#include "math_ops.h"
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int factorial(int n) {
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
+}
